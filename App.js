5b3458b5a0534088412dd8dfca6e0fd3a9a548f6
Melchizedek48 <67267755+Melchizedek48@users.noreply.github.com>
2020-12-14 11:12:09 +0800

    Set up a state variable

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [loading, setloading] = useState(true);
+
  return (
    <View style={styles.container}>
      <Text style={styles.title}>Bus arrival time:</Text>
-     <Text style={styles.arrivalTime}>Loading...</Text>
+     <Text style={styles.arrivalTime}>
+       {loading? "Loading..." : "Loaded"}
+       </Text>
      <TouchableOpacity style={styles.button}>
        <Text style={styles.buttonText}>Refresh!</Text>
      </TouchableOpacity>
